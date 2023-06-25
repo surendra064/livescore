190ab0274c751f5eb6027e112ca3c55564e0530e
surendra064 <129845824+surendra064@users.noreply.github.com>
2023-06-25 13:00:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from time import sleep
 
 
 def ascii_art():
-    # An artistic banner for the script
+    
     print("""
 ██╗     ██╗██╗   ██╗███████╗     ██████╗██████╗ ██╗ ██████╗██╗  ██╗███████╗████████╗    ███████╗ ██████╗ ██████╗ ██████╗ ███████╗
 ██║     ██║██║   ██║██╔════╝    ██╔════╝██╔══██╗██║██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝
@@ -18,17 +18,17 @@ def ascii_art():
 
 
 def get_current_matches():
-    # Function to fetch the live matches currently in play
-    page = requests.get('http://static.cricinfo.com/rss/livescores.xml')  # HTTP Get request to cricinfo rss feed
-    soup = BeautifulSoup(page.text, 'xml')  # parsing and building xml tree out of the returned xml file
-    matches = soup.find_all('description')  # description tags contain the score
+    
+    page = requests.get('http://static.cricinfo.com/rss/livescores.xml')  
+    soup = BeautifulSoup(page.text, 'xml')  
+    matches = soup.find_all('description')  
     live_matches = [s.get_text() for s in matches if
-                    '*' in s.get_text()]  # returns only the live matches and ignores the completed ones
+                    '*' in s.get_text()]  
     return live_matches
 
 
 def fetch_score(matchNum):
-    # Function to return the live score of the match specified
+    
     page = requests.get('http://static.cricinfo.com/rss/livescores.xml')
     soup = BeautifulSoup(page.text, 'xml')
     matches = soup.find_all('description')
@@ -37,7 +37,7 @@ def fetch_score(matchNum):
 
 
 def notify(score,support):
-    # Function for Windows toast desktop notification
+    
     toaster = ToastNotifier()
     toaster.show_toast(score,
                        f"Go {support},Go!",
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     matches = get_current_matches()
     print('Current matches in play')
     print('=' * 23)
-    # printing a list of all the current live matches
+
     for i, match in enumerate(matches):
         print('[{}] '.format(i) +
               re.search('\D+', match.split('v')[0]).group() + 'vs.' +
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     matchNum = int(input('Pick the match number [0,1,2...] => '))
     support=input("enter the team you are supporting")
 
-    # shows desktop notification every 30 seconds
+    
     while True:
         current_score = fetch_score(matchNum)
         notify(current_score,support)
